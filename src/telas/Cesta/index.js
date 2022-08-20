4733b9cedfb4bb0ad862b6210020f1a60bcd020e
DiegoGielda <diegogielda7@gmail.com>
2022-08-20 10:24:49 -0300

    Realizado criação da lista de itens com o (map)

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, View } from 'react-native';
+import { StyleSheet, ScrollView, Dimensions, View } from 'react-native';
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
+import Itens from './componentes/Itens';
 
-export default function Cesta({ topo, detalhes }) {
+export default function Cesta({ topo, detalhes, itens }) {
     return <>
-        <Topo {...topo} />
+        <ScrollView>
+            <Topo {...topo} />
 
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes} />
-        </View>
+            <View style={estilos.cesta}>
+                <Detalhes {...detalhes} />
+                <Itens {...itens} />
+            </View>
+        </ScrollView>
     </>
 }
 
